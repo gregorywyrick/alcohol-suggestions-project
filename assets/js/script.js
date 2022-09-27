e3758f6146a100e76aab990e7418628bdf83914c
Thorne <gthorne61@gmail.com>
2022-09-26 20:32:17 -0500

    api wine1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 //API Key: 7a70829eed4249369e766b2485bafa8d
-
+var wineSuggestionEl = document.getElementById("#");
+var wineResponeEl = document.getElementById("#"); 
 var requestUrl = "https://api.spoonacular.com/food/wine/recommendation?wine=red_wine&apiKey=7a70829eed4249369e766b2485bafa8d";
-
+//Cabernet, Pinot Grigio, Chardonnay, Merlot
 
 fetch(requestUrl)
 .then(function (response) {
@@ -10,3 +11,10 @@ fetch(requestUrl)
 .then(function (data) {
     console.log(data);
 });
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+var wineSuggestionEl.addEventListner("submit", formSubmitHandler);
